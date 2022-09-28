00653113403478bf30020306c4023336e30efe68
neeraj gupta official <neeraj.gupta@practo.com>
2022-09-28 18:35:58 +0530

    added references

--- src/main/java/com/practo/insta/hackaton/reporting/resource/PatientsResource.java
+++ src/main/java/com/practo/insta/hackaton/reporting/resource/PatientsResource.java
@@ -1,16 +1,13 @@
 package com.practo.insta.hackaton.reporting.resource;
 
-import com.practo.insta.hackaton.reporting.domain.PatientDetails;
+import com.practo.insta.hackaton.reporting.domain.PatientVisit;
 import com.practo.insta.hackaton.reporting.request.PatientRegistrationRequestBody;
 import com.practo.insta.hackaton.reporting.service.PatientRegistrationService;
 import io.swagger.annotations.Api;
-import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -23,7 +20,7 @@ public class PatientsResource {
     PatientRegistrationService patientRegistrationService;
 
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    private List<PatientDetails> indexPatients(@RequestBody PatientRegistrationRequestBody body){
+    private List<PatientVisit> indexPatients(@RequestBody PatientRegistrationRequestBody body){
         System.out.println(body);
         return patientRegistrationService.index(body.getFromDatetime(), body.getToDatetime(), true);
     }
